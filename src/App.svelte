756e5c89f41ddb1cee862db486f34ef0daf8f023
Jeffrey Vest <jeffrey.vest@gmail.com>
2019-12-11 14:55:16 -0500

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,91 @@
+<script>
+  let nextTodoText = "";
+  let selectedTodo = null;
+  let todos = JSON.parse(localStorage.getItem("todos")) || [];
+  let selectedTodoId = JSON.parse(localStorage.getItem("selectedTodoId")) || -1;
+
+  $: localStorage.setItem("todos", JSON.stringify(todos));
+
+  $: {
+    selectedTodo = todos.find(x => x.id === selectedTodoId);
+    localStorage.setItem("selectedTodoId", JSON.stringify(selectedTodoId));
+  }
+
+  function addTodo() {
+    todos = [
+      {
+        id: Date.now(),
+        text: nextTodoText,
+        done: false
+      },
+      ...todos
+    ];
+    nextTodoText = "";
+  }
+
+  function deleteTodo(todo) {
+    todos = todos.filter(x => x.id !== todo.id);
+  }
+
+  function selectTodo(todo) {
+    selectedTodoId = todo.id;
+  }
+
+  function completeTodo(todo) {
+    todos.find(x => x.id === todo.id).done = true;
+  }
+</script>
+
+<style>
+  main {
+    padding: 50px;
+    display: flex;
+  }
+
+  .column {
+    flex: 50%;
+  }
+
+  .nextTodo {
+    width: 500px;
+  }
+
+  .button {
+    cursor: pointer;
+  }
+
+  .complete-todos {
+    text-decoration-line: line-through;
+  }
+
+  img {
+    max-height: 800px;
+  }
+
+  .selected {
+    background-color: lightyellow;
+  }
+</style>
+
+<main>
+  <div class="column">
+    <form on:submit|preventDefault={addTodo} autocomplete="off">
+      <input class="nextTodo" type="text" bind:value={nextTodoText} />
+    </form>
+    {#each todos as todo (todo.id)}
+      <div
+        class:selected={selectedTodoId === todo.id}
+        on:click={() => selectTodo(todo)}>
+        <input type="checkbox" bind:checked={todo.done} />
+        <span class="button" on:click={() => deleteTodo(todo)}>❌</span>
+        <span class:complete-todos={todo.done}>{todo.text}</span>
+        <input type="text" bind:value={todo.imgSrc} />
+      </div>
+    {/each}
+  </div>
+  <div class="column">
+    {#if selectedTodo && selectedTodo.imgSrc}
+      <img src={selectedTodo.imgSrc} alt={selectedTodo.text + ' - image'} />
+    {/if}
+  </div>
+</main>
