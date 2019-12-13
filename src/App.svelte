52789079a1db4c504ac2330f63834bdd4cd5cc4b
Jeffrey Vest <jeffrey.vest@gmail.com>
2019-12-13 14:49:31 -0500

    Conversion to tailwind css.

--- src/App.svelte
+++ src/App.svelte
@@ -28,56 +28,27 @@
   }
 </script>
 
-<style>
-  main {
-    padding: 50px;
-    display: flex;
-  }
-
-  .column {
-    flex: 50%;
-  }
-
-  .nextTodo {
-    width: 500px;
-  }
-
-  .button {
-    cursor: pointer;
-  }
-
-  .complete-todos {
-    text-decoration-line: line-through;
-  }
-
-  img {
-    max-height: 800px;
-  }
-
-  .selected {
-    background-color: lightyellow;
-  }
-</style>
-
-<main>
-  <div class="column">
+<main class="container mx-auto flex">
+  <div class="w-1/2 pr-10">
     <form on:submit|preventDefault={addTodo} autocomplete="off">
-      <input class="nextTodo" type="text" bind:value={nextTodoText} />
+      <input class="w-full mb-3" type="text" bind:value={nextTodoText} />
     </form>
     {#each $todos as todo (todo.id)}
       <div
-        class:selected={$selectedTodoId === todo.id}
+        class:bg-gray-200={$selectedTodoId === todo.id}
         on:click={() => selectTodo(todo)}>
         <input type="checkbox" bind:checked={todo.done} />
-        <span class="button" on:click={() => deleteTodo(todo)}>❌</span>
-        <span class:complete-todos={todo.done}>{todo.text}</span>
+        <span class="cursor-pointer" on:click={() => deleteTodo(todo)}>❌</span>
+        <span class:line-through={todo.done}>{todo.text}</span>
         <input type="text" bind:value={todo.imgSrc} />
       </div>
     {/each}
   </div>
-  <div class="column">
+  <div class="w-1/2">
     {#if $selectedTodo && $selectedTodo.imgSrc}
-      <img src={$selectedTodo.imgSrc} alt={$selectedTodo.text + ' - image'} />
+      <div class="max-w-sm p-5 rounded-lg shadow-lg">
+        <img src={$selectedTodo.imgSrc} alt={$selectedTodo.text + ' - image'} />
+      </div>
     {/if}
   </div>
 </main>
